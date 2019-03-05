b1a225000e69cac9824cdc5d3357e4adc162288e
José Déniz <jodeniz@domingoalonsogroup.com>
2019-03-05 13:10:02 +0000

    ignore

--- rpgcombatkata/ListenerException.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace rpgcombatkata {
-    public class ListenerException : Exception{
-        public ListenerException(Exception innerException): base("", innerException){}
-    }
-}
